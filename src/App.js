ae443ce3b211cd20162901464619083bc8201043
Avadhut Thorat <avadhutthorat40@gmail.com>
2021-04-07 10:57:45 +0530

    Added Dockerfile

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React, { Component } from "react";
 import Todo from "./Todo";
 import UserInput from "./UserInput";
+import "./app.css";
 
 class App extends Component {
   state = {
     todo: [
       { id: 1, content: "buy milk" },
-      { id: 2, content: "create todo app" }
-    ]
+      { id: 2, content: "create todo app" },
+    ],
   };
 
-  removeTodoHandler = id => {
-    console.log(id);
-    const todo = this.state.todo.filter(todo => {
-      return todo.id !== id;
+  changeToInput = (id) => {
+    const todo = this.state.todo.map((todo) => {
+      if (todo.id === id) {
+        todo.editable = true;
+      }
+      return todo;
     });
+
     this.setState({ todo });
   };
 
-  addtodoHandler = content => {
+  saveData = (data) => {
+    const edit = this.state.todo.filter((todo) => {
+      return todo.editable;
+    });
+    console.log(edit);
+    const todo = [
+      ...this.state.todo,
+      { ...edit[0], content: data, editable: false },
+    ];
+  };
+
+  addtodoHandler = (content) => {
     console.log(content);
     content.id = Math.random() * 10;
     let todo = [...this.state.todo, content];
@@ -27,11 +42,68 @@ class App extends Component {
   };
 
   render() {
+    // const array = [1, 2, "Avadhut", 4];
+    // const [, , third] = array;
+    // console.log(third);
+    // const user = {
+    //   id: 1,
+    //   name: "John Doe",
+    //   address: {
+    //     city: {
+    //       name: "Pune",
+    //     },
+    //   },
+    // };
+    // const {
+    //   name,
+    //   address: {
+    //     city: { name: cityName },
+    //   },
+    // } = user;
+    // console.log(name, cityName);
+
+    const array = [
+      {
+        id: 1,
+        name: "John Doe",
+      },
+      {
+        id: 2,
+        name: "Jane Doe",
+      },
+      {
+        id: 3,
+        name: "Bob Marsh",
+      },
+      {
+        id: 4,
+        name: "John Doe",
+      },
+    ];
+
+    // const arr = [];
+
+    // array.map((item) => {
+    //   if (!arr.includes(item.name)) {
+    //     arr.push(item.name);
+    //   }
+    // });
+
+    const names = [...new Set(array.map((item) => item.name))];
+    console.log(names);
     return (
-      <div className="todo-app container">
-        <h1 className="center green-text">Todo's</h1>
-        <Todo todo={this.state.todo} removeTodo={this.removeTodoHandler} />
-        <UserInput addTodo={this.addtodoHandler} />
+      <div className="todo-app-container">
+        <div id="div1">Div1</div>
+        <div id="div2">Div2</div>
+        <div id="div3">Div3</div>
+        <div id="div4">Div4</div>
+        {/* <h1 className="center green-text">Todo's</h1>
+        <Todo
+          todo={this.state.todo}
+          changeToInput={this.changeToInput}
+          saveData={this.saveData}
+        />
+        <UserInput addTodo={this.addtodoHandler} /> */}
       </div>
     );
   }
--- src/Todo.js
+++ src/Todo.js
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Todo = ({ todo, removeTodo }) => {
+const Todo = ({ todo, changeToInput, saveData }) => {
+  console.log(todo);
+  const [name, setName] = useState("");
+  const onchangeHandler = (e) => {
+    setName(e.target.value);
+  };
+  const saveDataParent = (e) => {
+    e.preventDefault();
+    saveData(name);
+  };
   const todoList =
     todo.length > 0 ? (
-      todo.map(todo => {
+      todo.map((todo) => {
         return (
           <div className="collection-item" key={todo.id}>
-            <span
-              onClick={() => {
-                removeTodo(todo.id);
-              }}
-            >
-              {todo.content}
-            </span>
+            {!todo.editable ? (
+              <span
+                onClick={() => {
+                  changeToInput(todo.id);
+                }}
+              >
+                {todo.content}
+              </span>
+            ) : (
+              <form onSubmit={(e) => saveDataParent(e)}>
+                <input
+                  value={name || todo.content}
+                  type="text"
+                  onChange={onchangeHandler}
+                />
+              </form>
+            )}
           </div>
         );
       })
     ) : (
       <div className="center collection-item">You have no todo left !!</div>
     );
+
   return <div className="todos collection">{todoList}</div>;
 };
 
